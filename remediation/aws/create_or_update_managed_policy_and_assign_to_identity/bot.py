5a075170709902d70b7aadb7b8da860dfcee2f70
Joshua Bell <joshua.bell@sonraisecurity.com>
2020-08-07 13:56:01 -0300

    update get_data_enum_evidence to handle list and apply inline policies

--- remediation/aws/create_or_update_managed_policy_and_assign_to_identity/bot.py
+++ remediation/aws/create_or_update_managed_policy_and_assign_to_identity/bot.py
@@ -6,41 +6,70 @@ import logging
 
 
 def run(ctx):
-    # Get data for bot from config
-    config = ctx.config
-    data = ctx.get_analytic_evidence()
-    policy_to_apply = None
-    if "policy" in data:
-        policy_to_apply = json.dumps(data['policy'])
-    # Attach to identity
-    identity_arn = data['resourceId']
-    identity = sonrai.platform.aws.arn.parse(data['resourceId'])
+    # Get data for bot from ctx
+    data_enum = ctx.get_data_enum_evidence()
 
-    # Get role name
-    resource_arn = sonrai.platform.aws.arn.parse(data['policyResourceId'])
-    policy_arn = data['policyResourceId']
-    policy_name = resource_arn \
-        .assert_service("iam") \
-        .assert_type("policy") \
-        .name
+    # loop for each analytic data result
+    for data in data_enum:
 
-    logging.info('Creating or updating policy: {} and attaching to identity: {}'.format(policy_arn, identity_arn))
+        policy_to_apply = None
+        if "policy" in data:
+            policy_to_apply = json.dumps(data['policy'])
 
-    # Create AWS identity and access management client
-    iam_client = ctx.get_client().get('iam')
+        # Verify inline policy is to be applied with policyResourceId==NA
+        if data['policyResourceId'] == 'NA':
 
-    # Create or update policy
-    policy_arn = create_or_update_policy(iam_client=iam_client, policy_arn=policy_arn, policy_name=policy_name, policy_to_apply=policy_to_apply, identity=identity, resource_arn=resource_arn)
+            # identity
+            identity = sonrai.platform.aws.arn.parse(data['resourceId'])
+            policy_name = identity.name
 
-    # Attach to identity
-    if identity.resource_type == 'user':
-        iam_client.attach_user_policy(UserName=identity.name, PolicyArn=policy_arn)
-    elif identity.resource_type == 'group':
-        iam_client.attach_group_policy(GroupName=identity.name, PolicyArn=policy_arn)
-    elif identity.resource_type == 'role':
-        iam_client.attach_role_policy(RoleName=identity.name, PolicyArn=policy_arn)
-    else:
-        raise Exception('Expected identity to be of resource type (user/role/group)')
+            client = ctx.get_client().get(identity.service)
+
+            if identity.service == 'iam':
+                if identity.resource_type == 'user':
+                    client.put_user_policy(UserName=identity.name, PolicyName=policy_name, PolicyDocument=policy_to_apply)
+                elif identity.resource_type == 'group':
+                    client.put_group_policy(GroupName=identity.name, PolicyName=policy_name, PolicyDocument=policy_to_apply)
+                elif identity.resource_type == 'role':
+                    client.put_role_policy(RoleName=identity.name, PolicyName=policy_name, PolicyDocument=policy_to_apply)
+            elif identity.service == 'kms':
+                client.put_key_policy(KeyId=identity.name, PolicyName=policy_name, PolicyDocument=policy_to_apply)
+            elif identity.service == 's3':
+                client.put_bucket_policy(Bucket=identity.name, ConfirmRemoveSelfBucketAccess=True, Policy=policy_to_apply)
+            else:
+                raise Exception('service of type {} is not supported for inline policy remediation'.format(identity.service))
+
+        else:
+
+            # Attach to identity
+            identity_arn = data['resourceId']
+            identity = sonrai.platform.aws.arn.parse(data['resourceId'])
+
+            # Get role name
+            resource_arn = sonrai.platform.aws.arn.parse(data['policyResourceId'])
+            policy_arn = data['policyResourceId']
+            policy_name = resource_arn \
+                .assert_service("iam") \
+                .assert_type("policy") \
+                .name
+
+            logging.info('Creating or updating policy: {} and attaching to identity: {}'.format(policy_arn, identity_arn))
+
+            # Create AWS identity and access management client
+            iam_client = ctx.get_client().get('iam')
+
+            # Create or update policy
+            policy_arn = create_or_update_policy(iam_client=iam_client, policy_arn=policy_arn, policy_name=policy_name, policy_to_apply=policy_to_apply, identity=identity, resource_arn=resource_arn)
+
+            # Attach to identity
+            if identity.resource_type == 'user':
+                iam_client.attach_user_policy(UserName=identity.name, PolicyArn=policy_arn)
+            elif identity.resource_type == 'group':
+                iam_client.attach_group_policy(GroupName=identity.name, PolicyArn=policy_arn)
+            elif identity.resource_type == 'role':
+                iam_client.attach_role_policy(RoleName=identity.name, PolicyArn=policy_arn)
+            else:
+                raise Exception('Expected identity to be of resource type (user/role/group)')
 
 
 def create_or_update_policy(iam_client=None, policy_arn=None, policy_name=None, policy_to_apply=None, identity=None, resource_arn=None):

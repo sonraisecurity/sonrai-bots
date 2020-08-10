89bda9936ea7f85d15233ad2f0f6e68ce7a617b9
Taylor Osmun <taylor.osmun@sonraisecurity.com>
2020-08-10 14:24:56 -0300

    Update bot.py (#22)

--- remediation/azure/delete_service_principal/bot.py
+++ remediation/azure/delete_service_principal/bot.py
@@ -9,23 +9,18 @@ def run(ctx):
     object_id = ctx.resource_id
 
     graphrbac_client = ctx.get_client().get(GraphRbacManagementClient)
-    hasMatch = False
-
     for object in graphrbac_client.service_principals.list():
-
-        if object.object_id == object_id and object.service_principal_type == 'Application':
-            application = next(graphrbac_client.applications.list(filter="appId eq '{}'".format(object.app_id)))
-            logging.info('deleting App registration: {}'.format(application.object_id))
-            graphrbac_client.applications.delete(application_object_id=application.object_id)
-            hasMatch = True
-            break
-
-        elif object.object_id == object_id and object.service_principal_type == 'ManagedIdentity':
-            managed_identity_client = ctx.get_client().get(ManagedIdentityClient)
-            if object.alternative_names[0] == 'isExplicit=True':
-                managed_identity_client.delete(object_path=object.alternative_names[1])
-                hasMatch = True
-                break
-
-    if not hasMatch:
-        raise Exception('object_id: {} did not match any service principals'.format(object_id))
+        if object.object_id == object_id:
+            if object.service_principal_type == 'Application':
+                application = next(graphrbac_client.applications.list(filter="appId eq '{}'".format(object.app_id)))
+                logging.info('deleting App registration: {}'.format(application.object_id))
+                graphrbac_client.applications.delete(application_object_id=application.object_id)
+                return
+            elif object.service_principal_type == 'ManagedIdentity':
+                managed_identity_client = ctx.get_client().get(ManagedIdentityClient)
+                if object.alternative_names[0] == 'isExplicit=True':
+                    logging.info('deleting Managed Identity: {}'.format(object.alternative_names[1]))
+                    managed_identity_client.delete(object_path=object.alternative_names[1])
+                    return
+                raise Exception('object_id: {} is a ManagedIdentity, but had no explicit alternative name'.format(object_id))
+    raise Exception('object_id: {} did not match any service principals'.format(object_id))

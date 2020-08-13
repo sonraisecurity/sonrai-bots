45886886e1235d8e751754b245a527252e1f6e87
Joshua Bell <joshua.bell@sonraisecurity.com>
2020-08-13 13:59:45 -0300

    Combine delete bots for GCP/Azure to remove permissions bots

--- remediation/azure/remove_user_permissions/bot.py
+++ remediation/azure/remove_user_permissions/bot.py
@@ -1,6 +1,7 @@
 import logging
 
 from azure.graphrbac import GraphRbacManagementClient
+from azure.graphrbac.models import GraphErrorException
 
 from sonrai.platform.azure.client import ManagedIdentityClient
 
@@ -9,6 +10,17 @@ def run(ctx):
     object_id = ctx.resource_id
 
     graphrbac_client = ctx.get_client().get(GraphRbacManagementClient)
+
+    try:
+        graphrbac_client.users.get(upn_or_object_id=object_id)
+        logging.info('deleting user: {}'.format(object_id))
+        graphrbac_client.users.delete(upn_or_object_id=object_id)
+    except GraphErrorException as e:
+        if e.error.code == 'Request_ResourceNotFound':
+            pass
+        else:
+            raise e
+
     for object in graphrbac_client.service_principals.list():
         if object.object_id == object_id:
             if object.service_principal_type == 'Application':
@@ -22,5 +34,7 @@ def run(ctx):
                     logging.info('deleting Managed Identity: {}'.format(object.alternative_names[1]))
                     managed_identity_client.delete(object_path=object.alternative_names[1])
                     return
-                raise Exception('object_id: {} is a ManagedIdentity, but had no explicit alternative name'.format(object_id))
-    raise Exception('object_id: {} did not match any service principals'.format(object_id))
+                raise Exception(
+                    'object_id: {} is a ManagedIdentity, but had no explicit alternative name'.format(object_id))
+
+    raise Exception('object_id: {} did not match any service principals or users'.format(object_id))

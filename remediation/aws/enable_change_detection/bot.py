328968ab79dc81906370fb761d844bc5bfd84b71
nnayaju <nimesh.nayaju@sonraisecurity.com>
2020-08-26 10:11:28 -0300

    Better logging and remove exception handling

--- remediation/aws/enable_change_detection/bot.py
+++ remediation/aws/enable_change_detection/bot.py
@@ -7,7 +7,7 @@ def run(ctx):
     # Create GraphQL client
     graphql_client = ctx.graphql_client()
 
-    # GraphQL query to enable change detection on resource with the SRN specified
+    # GraphQL query to enable change detection on resource with the specified SRN
     query = '''
         mutation EnableChangeDetection($srn: String!) {
           setMonitor(
@@ -25,11 +25,10 @@ def run(ctx):
     '''
     variables = {"srn": object_srn}
 
-    try:
-        graphql_client.query(query, variables)
-        logging.info('enabling change detection on: {}'.format(object_srn))
-    except IOError as e:
-        pass
+    # Enable change detection
+    logging.info('enabling change detection on: {}'.format(object_srn))
+    graphql_client.query(query, variables)
+
 
 
 

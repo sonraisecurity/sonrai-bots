94ca6f4f0c35983b4d4fc14a273a8983638eb3e9
nnayaju <nimesh.nayaju@sonraisecurity.com>
2020-08-25 21:08:25 -0300

    feat: SAR-1375 Add turn_on_crm bot that enables change detection on a resource

---
+++ remediation/graphql/turn_on_crm/bot.py
@@ -0,0 +1,35 @@
+import logging
+
+
+def run(ctx):
+    object_srn = ctx.resource_srn
+
+    # Create GraphQL client
+    graphql_client = ctx.graphql_client()
+
+    # GraphQL query to enable change detection on resource with the SRN specified
+    query = '''
+        mutation TurnOnCRM($srn: String!) {
+          setMonitor(
+            monitorStatusBySrn: [
+              {
+                srn: $srn,
+                monitor: true
+              }
+            ]
+          ) {
+            srn
+            monitor
+          }
+        }
+    '''
+    variables = {"srn": object_srn}
+
+    try:
+        graphql_client.query(query, variables)
+        logging.info('enabling change detection on: {}'.format(object_srn))
+    except IOError as e:
+        pass
+
+
+

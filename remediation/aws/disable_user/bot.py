8336aa400198eeadae329aca6a36f0efe5f6627a
Taylor Osmun <taylor.osmun@sonraisecurity.com>
2020-12-21 10:47:01 -0400

    Accounting for pages in access key response

--- remediation/aws/disable_user/bot.py
+++ remediation/aws/disable_user/bot.py
@@ -2,21 +2,33 @@ import botocore
 import logging
 import sonrai.platform.aws.arn
 
+log = logging.getLogger()
+
+def _responses(f, *args, **kwargs):
+    while True:
+        response = f(*args, **kwargs)
+        yield response
+        if not response["IsTruncated"]:
+            return
+        marker = response["Marker"]
+        if not marker:
+            return
+        kwargs["Marker"] = marker
+
 def run(ctx):
     # Create AWS identity and access management client
-    iam_client = ctx.get_client().get('iam')
-
+    client = ctx.get_client().get('iam')
     # Verify that the resource_id is an IAM User
     #   arn:aws:iam::123271222534:user/testUser
-    resource_arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
-    user_name = resource_arn \
+    user_arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    user_name = user_arn \
         .assert_service("iam") \
         .assert_type("user") \
         .name
-
     # Delete login profile
+    log.info("[{}] Deleting login profile, if any".format(user_arn))
     try:
-        iam_client.delete_login_profile(UserName=user_name,)
+        client.delete_login_profile(UserName=user_name,)
     except botocore.exceptions.ClientError as e:
         error = e.response.get("Error")
         # AWS User may not have a login profile already
@@ -24,6 +36,10 @@ def run(ctx):
             raise e
 
     # Delete access keys
-    access_key_list = iam_client.list_access_keys(UserName=user_name, )
-    for accessKey in access_key_list['AccessKeyMetadata']:
-        iam_client.delete_access_key(UserName=user_name, AccessKeyId=accessKey['AccessKeyId'])
+    for response in _responses(client.list_access_keys, UserName=user_name):
+        for access_key in response['AccessKeyMetadata']:
+            access_key_id = access_key['AccessKeyId']
+            log.info("[{}] Deleting access key: {}".format(user_arn, access_key_id))
+            client.delete_access_key(UserName=user_name, AccessKeyId=access_key_id)
+
+    log.info("[{}] User disabled".format(user_arn))

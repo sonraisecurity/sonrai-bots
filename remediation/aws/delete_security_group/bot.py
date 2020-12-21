1f151f2b1aaa7939b263800645bc5a0976be22d7
joshua laver <joshua.laver@sonraisecurity.com>
2020-12-21 17:43:59 -0400

    feat: SAR-3165 AWS Delete Security Group Bot

---
+++ remediation/aws/delete_security_group/bot.py
@@ -0,0 +1,20 @@
+import logging
+import sonrai.platform.aws.arn
+
+
+def run(ctx):
+    # Create AWS EC2 client
+    ec2_client = ctx.get_client().get('ec2')
+
+    # Parse resource id to group
+    resource_id = ctx.resource_id
+    resource_arn = sonrai.platform.aws.arn.parse(resource_id)
+    group_id = resource_arn \
+        .assert_service("ec2") \
+        .assert_type("security-group") \
+        .resource
+
+    # Refers doc https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DeleteSecurityGroup.html
+    response = ec2_client.delete_security_group(GroupId=group_id,)
+    logging.info('deleted security group: {}'.format(response))
+

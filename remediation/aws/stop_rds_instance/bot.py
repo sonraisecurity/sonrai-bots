66d65d1b9d370b9936d25b82e28af92ce7ce05dc
Lin Yang <lin.yang@sonraisecurity.com>
2020-12-21 12:00:26 -0400

    Add start/stop rds instance bots

---
+++ remediation/aws/stop_rds_instance/bot.py
@@ -0,0 +1,13 @@
+import sonrai.platform.aws.arn
+
+def run(ctx):
+
+    # Get role name
+    resource_arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    instance_id = resource_arn \
+        .assert_service("rds") \
+        .assert_type("db") \
+        .name
+
+    rds_client = ctx.get_client().get('rds', resource_arn.region)
+    rds_client.stop_db_instance(DBInstanceIdentifier=instance_id)

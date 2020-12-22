1ac7647053e0badb798aa66aff33057d610bd51a
Mark Moorcroft <mark.moorcroft@sonraisecurity.com>
2020-12-22 13:36:57 -0400

    feat: SAR-1370 Specify region on client

--- remediation/aws/delete_vpc/bot.py
+++ remediation/aws/delete_vpc/bot.py
@@ -3,13 +3,12 @@ import logging
 import sonrai.platform.aws.arn
 
 def run(ctx):
-    resource_id = ctx.resource_id
-    resource_arn = sonrai.platform.aws.arn.parse(resource_id)
-    vpcid = resource_arn \
+    arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    vpcid = arn \
         .assert_service("ec2") \
         .assert_type("vpc") \
         .resource
     logging.info('Removing VPC ({}) from AWS'.format(vpcid))
-    ec2client = ctx.get_client().get('ec2')
+    ec2client = ctx.get_client().get('ec2', region=arn.region)
     response = ec2client.delete_vpc(VpcId=vpcid)
     logging.info('Removed VPC: {}'.format(response))

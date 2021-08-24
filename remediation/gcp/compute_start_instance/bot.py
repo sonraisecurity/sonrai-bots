6e3ac3c42a92d7a480a7e9405381320035291d1c
Taylor Osmun <taylor.osmun@sonraisecurity.com>
2021-08-24 15:55:56 -0300

    Adds `GCP Compute - Start Instance`

---
+++ remediation/gcp/compute_start_instance/bot.py
@@ -0,0 +1,23 @@
+import re
+import logging
+
+_PATTERN = re.compile('^//compute.googleapis.com/projects/([^/]+)/zones/([^/]+)/instances/([^/]+)$', re.IGNORECASE)
+
+
+def run(ctx):
+    m = _PATTERN.match(ctx.resource_id)
+    if not m:
+        raise ValueError("Invalid resource_id: {}".format(ctx.resource_id))
+    project_name = m.group(1)
+    zone_name = m.group(2)
+    instance_id = m.group(3)
+    client_kwargs = {
+        "project": project_name,
+        "zone": zone_name,
+        "instance": instance_id
+    }
+    log_prefix = "[{}] ".format(client_kwargs)
+    # Lookup instance metadata
+    client = ctx.get_client().get('compute', 'v1')
+    logging.info(log_prefix + "Starting instance")
+    client.instances().start(**client_kwargs).execute()

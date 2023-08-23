74556a740a78756c769d5dae00038ee3fad6599c
Jacob Pembleton <jacob.pembleton@sonraisecurity.com>
2023-08-23 14:17:18 -0300

    feat(SAR-20048): Add bot to delete stacks
    This commit adds a bot to delete CloudFormation Stacks.

---
+++ remediation/aws/delete_cloudformation_stack/bot.py
@@ -0,0 +1,14 @@
+import sonrai.platform.aws.arn
+
+import logging
+
+def run(ctx):
+    # Create AWS CloudFormation client
+    cf_client = ctx.get_client().get('cloudformation')
+
+    stack_resource_srn = ctx.resource_srn
+
+    stack_name = stack_resource_srn.split('/')[-1]
+
+    logging.info(f'Deleting stack {stack_name}.')
+    cf_client.delete_stack(StackName=stack_name)

8f1da51159c306c1e6a1525f87c708a68af21dc1
Jacob Pembleton <jacob.pembleton@sonraisecurity.com>
2023-09-05 10:17:50 -0300

    clean up code, specify region

--- remediation/aws/delete_cloudformation_stack/bot.py
+++ remediation/aws/delete_cloudformation_stack/bot.py
@@ -3,12 +3,14 @@ import sonrai.platform.aws.arn
 import logging
 
 def run(ctx):
+    arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    stack_name = arn \
+        .assert_service('cloudformation') \
+        .assert_type('stack') \
+        .resource
+
     # Create AWS CloudFormation client
-    cf_client = ctx.get_client().get('cloudformation')
+    cf_client = ctx.get_client().get('cloudformation', region=arn.region)
 
-    stack_resource_srn = ctx.resource_srn
-
-    stack_name = stack_resource_srn.split('/')[-2]
-
-    logging.info(f'Deleting stack {stack_name}.')
+    logging.info(f'Deleting stack {stack_name} in region {arn.region}')
     cf_client.delete_stack(StackName=stack_name)

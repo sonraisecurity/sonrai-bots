0efacc7b189b680ed507e5da1bb55b6772315dc1
gbizeau <gbizeau@gmail.com>
2023-10-02 15:43:26 -0300

    feat(SAR-23609): remediation bot to update swimlanes automagically

---
+++ remediation/any/swimlane_updater/bot.py
@@ -0,0 +1,133 @@
+import datetime
+import logging
+import time
+from datetime import datetime, timedelta
+
+def run(ctx):
+    # Create GraphQL client
+    graphql_client = ctx.graphql_client()
+
+    # load results of saved search - see graphql/Users.sql
+    logging.info('Loading User Results')
+
+    # Swimlane update search
+    sus_results = graphql_client.query('''
+    query {
+      Swimlanes ( where: { tags: { op: CONTAINS value: "|update-search" } } )
+      {
+        count
+        items(limit: -1) {
+          title
+          srn
+          tags
+          accounts
+          resourceIds
+        }
+      }
+    }
+    ''')
+
+    # print(sus_results)
+
+    # Iterate through each swimlane
+    for item in sus_results['data']['Swimlanes']['items']:
+        # Dictionary for update
+        # Keys: R = Resource IDs, A = Accounts, N = Names, T = Tags
+        updates = {"R": [], "A": [], "N": [], "T": []}
+
+        # Dictionary for swimlane
+        # Keys: R = Resource IDs, A = Accounts, N = Names, T = Tags
+        swimlane = {"R": [], "A": [], "N": [], "T": []}
+
+        # store the swimlane accounts
+        if item.get('accounts'):
+            # Create a list to store these
+            accountList = list()
+            for account in item['accounts']:
+                accountList.append(account)
+
+            # add the temp list into the swimlane dict
+            if accountList:
+                swimlane['A'] = accountList
+
+        # store the swimlane resources
+        if item.get('resourceIds'):
+            # Create a list to store these
+            resourceList = list()
+            for resource in item['resourceIds']:
+                resourceList.append(resource)
+
+            # add the temp list into the swimlane dict
+            if resourceList:
+                swimlane['R'] = resourceList
+
+        # Check tags for "update-search" key and iterate
+        if item.get('tags'):
+            for tag in item['tags']:
+                data = tag.split(":")
+                if '|update-search' in str(data[0][1:]):
+                    search = str(data[1][:-1])
+                    search_keys = str(data[0][1:]).split("|")
+                    search_type = str(search_keys[0]).upper()
+                    search_group = str(search_keys[1])
+                    return_value = str(search_keys[2])
+
+                    # some generic test for a valid search string
+                    if search and search.isascii() and search != '*' and len(search_keys) == 4:
+                        # Execute the search
+                        search_results = graphql_client.query('{{ExecuteSavedQuery {{Query (name:"{search}" )}}}}'.format(search=search))
+
+                        try:
+                            for row in search_results['data']['ExecuteSavedQuery']['Query'][search_group]['items']:
+                                tempR = list(updates[search_type])
+                                if search_type == 'R':
+                                    tempR.append('*' + row[return_value] + '*')
+                                else:
+                                    tempR.append(row[return_value])
+
+                                # Remove duplicates: list->set->list
+                                updates[search_type] = list(set(tempR))
+
+                        except KeyError:
+                            print("Invalid Search or Search_Group in tag: {search_keys}:{search}".format(search_keys=data[0], search=data[1]))
+
+                    else:
+                        print("Invalid Search or Search_Group in tag: {search_keys}:{search}".format(search_keys=data[0], search=data[1]))
+
+        # create the REMOVE and ADD arrays and update the swimlane
+        remove_resourceIds = list()
+        add_resourceIds = list()
+        remove_accounts = list()
+        add_accounts = list()
+
+        for key in updates:
+            if updates[key]:
+                add_resourceIds = list(set(updates[key]) - set(swimlane[key]))
+                remove_resourceIds = list(set(swimlane[key]) - set(updates[key]))
+
+        for key in swimlane:
+            if swimlane[key]:
+                add_accounts = list(set(updates[key]) - set(swimlane[key]))
+                remove_accounts = list(set(swimlane[key]) - set(updates[key]))
+
+        # Build the mutations
+
+        # Resources
+        add_resourceIds = str(add_resourceIds).replace("'", '"')
+        remove_resourceIds = str(remove_resourceIds).replace("'", '"')
+        resource = '{{ resourceIds: {{ add: {add}, remove: {remove} }}'.format(add=add_resourceIds, remove=remove_resourceIds)
+
+        # Accounts
+        add_accounts = str(add_accounts).replace("'", '"')
+        remove_accounts = str(remove_accounts).replace("'", '"')
+        accounts = ' accounts: {{ add: {add}, remove: {remove} }} }}'.format(add=add_accounts, remove=remove_accounts)
+
+        swimlane_mutation = 'mutation updateSwimlane {{ UpdateSwimlane(srn: "{srn}", value: {resource} {accounts} ) {{ srn }}}}'.format(srn=item['srn'], resource=resource, accounts=accounts)
+        if len(add_resourceIds) > 2 or len(remove_resourceIds) > 2 or len(add_accounts) > 2 or len(remove_accounts) > 2:
+            swimlane_results = graphql_client.query(swimlane_mutation)
+            print("Swimlane Update: ", item['title'], swimlane_results)
+
+            print("ADD: ", add_resourceIds)
+            print("DEL: ", remove_resourceIds)
+            print("ADD: ", len(add_accounts))
+            print("DEL: ", len(remove_accounts))
